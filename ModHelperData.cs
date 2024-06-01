a53890563ff4f81dcf28fa26742a4e76066d9bef
hubor0 <minecraftcostam2009@gmail.com>
2024-06-01 17:42:00 +0200

    fix

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,7 +8,7 @@ public static class ModHelperData
 
     public const string Description = "Adds a new custom Boxer Monkey tower. Complete with all 15 tiers and a Paragon coming soon!";
 
-    public const string RepoOwner = "hubor"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoOwner = "hubor0"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "boxer"; // TODO add your repo name here, also in the download url in README.md
     public const string Author = "hubor";
 }
